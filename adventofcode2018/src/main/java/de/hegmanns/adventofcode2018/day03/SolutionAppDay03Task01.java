acdfde0e31c57dd2167027c488639a935d808944
Bernd Hegmanns <it@hegmanns.de>
2018-12-09 09:05:56 +0100

    delete unused var

--- adventofcode2018/src/main/java/de/hegmanns/adventofcode2018/day03/SolutionAppDay03Task01.java
+++ adventofcode2018/src/main/java/de/hegmanns/adventofcode2018/day03/SolutionAppDay03Task01.java
@@ -1,14 +1,9 @@
 package de.hegmanns.adventofcode2018.day03;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class SolutionAppDay03Task01 {
 
 	public static void main(String[] args) {
-		
 		String[] strings = Input03.getStrings();
-		List<Rectangle> rectangles = new ArrayList<>();
 		Fabric fabric = new Fabric();
 		for (String string : strings) {
 			Rectangle rectangle = new Rectangle(string);
